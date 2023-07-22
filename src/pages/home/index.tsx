ebc4a4e862a9d00346c85ff210ae5a6f0c7347b2
sakshamjaiswal1 <sjais973@gmail.com>
2023-07-22 12:28:18 +0530

    initial

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/common/footer";
+import Header from "../../components/common/header";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
