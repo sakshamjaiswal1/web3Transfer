75cd4598a9ef01000876cf690b29a037fccff8ba
sakshamjaiswal1 <sjais973@gmail.com>
2023-07-24 17:28:21 +0530

    footer-added

--- src/components/common/footer/footerArbItem.tsx
+++ src/components/common/footer/footerArbItem.tsx
@@ -2,26 +2,34 @@ import copyLogo from "../../../assets/copy.svg";
 import redirect from "../../../assets/redirect.svg";
 import downArrowBlack from "../../../assets/down-arrow-black.svg";
 
-function footerArbItem({
+function FooterArbItem({
   title,
   imageUrl,
-  issDownArrow,
+  isDownArrow,
+  className = "",
 }: {
   title: string;
   imageUrl: string;
-  issDownArrow?: boolean;
+  isDownArrow?: boolean;
+  className?: string;
 }) {
   return (
-    <div>
+    <div className={`flex items-center ${className} text-[#404b51]`}>
       <img src={imageUrl} alt="" />
-      <p className="" >{title}</p>
-      {issDownArrow && (
-        <img src={downArrowBlack} alt="" className="w-[14px] h-[8px]" />
+      <p className="pl-[10px] whitespace-nowrap typo-footer-arb-text">
+        {title}
+      </p>
+      {isDownArrow && (
+        <img
+          src={downArrowBlack}
+          alt=""
+          className="w-[14px] h-[8px] ml-[12px]"
+        />
       )}
-      <img src={redirect} alt="" className="h-[18px] w-[18px]" />
-      <img src={copyLogo} alt="" className="h-[18px] w-[18px]" />
+      <img src={redirect} alt="" className="h-[18px] w-[18px] ml-[15px]" />
+      <img src={copyLogo} alt="" className="h-[18px] w-[18px] ml-[17px]" />
     </div>
   );
 }
 
-export default footerArbItem;
+export default FooterArbItem;
--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -6,6 +6,16 @@ import twitterLogo from "../../../assets/footer/twitter.svg";
 import dmLogo from "../../../assets/footer/dm.svg";
 import githubLogo from "../../../assets/footer/github.svg";
 import otherLogo from "../../../assets/footer/other.svg";
+import FooterArbItem from "./footerArbItem";
+import usdLogo from "../../../assets/usd_logo.svg";
+import spaLogo from "../../../assets/spa_logo.svg";
+import veSpaLogo from "../../../assets/veSpa_logo.svg";
+import wspaLogo from "../../../assets/wspa_Logo.svg";
+import mLogo from "../../../assets/m_logo.svg";
+import birdLogo from "../../../assets/bird_logo.svg";
+import swapLogo from "../../../assets/swap_logo.svg";
+import bridgelogo from "../../../assets/bridge_logo.svg";
+import rightArrow from "../../../assets/footer/right-arrow.svg";
 
 function Footer() {
   return (
@@ -25,10 +35,67 @@ function Footer() {
             <img src={otherLogo} alt="" className="h-[16px] w-[28px]" />
           </div>
         </div>
+        <div className="flex  w-full items-start gap-x-[84px] h-full mt-[68px] mb-[55px]">
+          <div className="flex flex-col justify-start">
+            <FooterArbItem title="USDs Arbitrum" imageUrl={usdLogo} />
+            <div className="mt-[32px] flex items-center gap-x-[18px]">
+              <img src={mLogo} alt="" className="h-[24px] w-[24px]" />
+              <img src={birdLogo} alt="" className="h-[24px] w-[24px]" />
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-start">
+            <FooterArbItem
+              title="SPA Arbitrum"
+              imageUrl={spaLogo}
+              isDownArrow
+            />
+            <div className="mt-[32px] flex items-center gap-x-[18px]">
+              <img src={mLogo} alt="" className="h-[24px] w-[24px]" />
+              <img src={birdLogo} alt="" className="h-[24px] w-[24px]" />
+            </div>
+          </div>
+          <div className="flex flex-col justify-start h-full">
+            <FooterArbItem
+              title="veSPA Arbitrum"
+              imageUrl={veSpaLogo}
+              isDownArrow
+            />
+          </div>
+          <div className="flex flex-col justify-start">
+            <FooterArbItem title="wSPA Ethereum" imageUrl={wspaLogo} />
+            <p className="typo-footer-arb-text flex items-center mt-[32px] text-[#404b51]">
+              {" "}
+              <img
+                src={swapLogo}
+                alt=""
+                className="h-[24px] w-[24px] mr-[10px]"
+              />{" "}
+              <span>SPA</span>{" "}
+              <img
+                src={rightArrow}
+                alt=""
+                className="h-[20px] w-[20px] mx-[8px]"
+              />{" "}
+              <span>wSPA</span>
+            </p>
+            <p className="typo-footer-arb-text flex items-center gap-x-[10px] mt-[16px] text-[#404b51]">
+              <img src={bridgelogo} className="h-[24px] w-[24px]" alt="" />{" "}
+              <span>Sperax- Arbitrum Bridge </span>{" "}
+            </p>
+          </div>
+        </div>
         <div className="flex  w-full items-center justify-between ">
-          <p>© 2023 Sperax Inc. All rights reserved.</p>
-          <div>
-            <p>Terms of Service</p> <p>Privacy Policy</p>
+          <p className="typo-footer-bottom-right text-[#ABB7BD] mr-[17px]">
+            © 2023 Sperax Inc. All rights reserved.
+          </p>
+          <div className="flex items-center typo-footer-arb-text">
+            <p>Terms of Service</p>{" "}
+            <p className="flex items-center gap-x-[12px]">
+              {" "}
+              <span className="rounded-[50%] h-[6px] w-[6px] bg-[#C8CFD3]" />
+              <span>Privacy Policy</span>{" "}
+            </p>
           </div>
         </div>
       </div>
